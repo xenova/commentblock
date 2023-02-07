1a09800a41b75ec7fbd5f49269bee0fcd7c7a3b3
Joshua Lochner <admin@xenova.com>
2023-02-07 14:17:09 +0200

    Improve preprocessing function

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,24 +6,29 @@ from detection_rules import BUFFER_CHARS, remove_unicode_categories
 
 
 def normalise(string):
+    # 0. Make sure it is a string
+    string = str(string)
 
-    # 0. Deconstruct emojies into text
+    # 1. Deconstruct emojies into text
     # Needed since we want to extract the semantic meaning from the emojis,
     # as opposed to <UNK>, which many tokenizers will assign to it
     string = emoji.demojize(string)
 
-    # 1. Replace strange unicode characters with most similar ASCII character
-    string = unicodedata.normalize('NFKD', str(string))
+    # 2. Replace strange unicode characters with most similar ASCII character
+    string = unicodedata.normalize('NFKD', string)
 
-    # 2. Replace all whitespace with a single space
-    string = ' '.join(string.split())
+    # 3. Remove certain types of unicode categories, like accents
+    string = remove_unicode_categories(string)
 
-    # 3. Remove specific duplicated characters (https://stackoverflow.com/a/49695605)
+    # 4. Replace all whitespace with a single space
+    string = ' '.join(string.strip().split())
+
+    # 5. Remove specific duplicated characters (https://stackoverflow.com/a/49695605)
     string = ''.join(k if k in BUFFER_CHARS else ''.join(v)
                      for k, v in itertools.groupby(string, lambda c: c))
 
-    # 4. Remove certain types of unicode categories, like colours
-    string = remove_unicode_categories(string)
+    # 6. Lowercase the string
+    string = string.lower()
 
     return string
 
@@ -32,7 +37,8 @@ def preprocess_single(author_name, comment_text):
     author_name = normalise(author_name)
     comment_text = normalise(comment_text)
 
-    return f'{author_name}\n{comment_text}'
+    # TODO add custom token?
+    return f'{author_name} commented {comment_text}'
 
 
 def preprocess_batch(author_names, comment_texts):
