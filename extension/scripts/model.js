63f90c24d1f41d74a76febd99bdd4ba0aad3dc05
Joshua Lochner <admin@xenova.com>
2023-02-08 22:12:43 +0200

    Replace `loadModel` function with `ModelFactory` design pattern

--- extension/scripts/model.js
+++ extension/scripts/model.js
@@ -1,13 +1,30 @@
 
-async function loadModel() {
-    ///////////// WORKS /////////////
-    const tokenizerURL = chrome.runtime.getURL("model/tokenizer.json");
-    const modelURL = chrome.runtime.getURL("model/model.onnx");
+let ModelFactory = (function () {
+    // https://stackoverflow.com/a/4842961
 
-    let model = new Model(tokenizerURL, modelURL)
-    await model.load() // Wait for model to load fully
-    return model;
-}
+    async function createModelPromise() {
+        console.log('Start loading model');
+        // Load the tokenizer and model.
+        const tokenizerURL = chrome.runtime.getURL("model/tokenizer.json");
+        const modelURL = chrome.runtime.getURL("model/model.onnx");
+        
+        let model = new Model(tokenizerURL, modelURL);
+        await model.load() // Wait for model to load fully
+        console.log('Model finished loading');
+        return model
+    }
+
+    let instance;
+    return {
+        getInstance: async function () {
+            if (instance == null) {
+                instance = createModelPromise();
+            }
+            return await instance;
+        }
+    };
+})();
+
 
 class Model {
     constructor(tokenizerURL, modelURL) {
@@ -56,7 +73,7 @@ class Model {
             attention_mask: attention_mask
         }
     }
-    preprocess(authorName, commentText){
+    preprocess(authorName, commentText) {
         // Normalise author name and comment text
         authorName = this.tokenizer.normalize(authorName);
         commentText = this.tokenizer.normalize(commentText);
@@ -84,67 +101,6 @@ class Model {
 
     }
 }
-(async () => {
-    // let url = chrome.runtime.getURL('model/tokenizer.json');
-    // const tokenizer = await AutoTokenizer.fromPretrained(url);
-    // console.log('tokenizer', tokenizer);
-
-    // // Translate "Hello, world!"
-    // const english = "Hello, world!";
-    // const inputTokenIds = tokenizer.encode("translate English to French: " + english);
-    // console.log('inputTokenIds', inputTokenIds);
-
-
-
-    // const url = chrome.runtime.getURL('model/tokenizer.json');
-    // const response = await fetch(url);
-
-    // let v = await response.json();
-
-    // let tempVocab = {};
-    // // Reverse vocab
-    // for (const [key, value] of Object.entries(v.model.vocab)) {
-    //     tempVocab[parseInt(value)] = key;
-    // }
-    // let vocab = Object.values(tempVocab)
-
-    // console.log('vocab', vocab);
-    // let t = new BertWordPieceTokenizer({
-    //     vocabContent: vocab
-    // })
-    // console.log(t);
-    // console.log(t.tokenizeSentence('The above name is the best when it comes to Bitcoin wallet recovery and recovering of lost funds. I lost $3000 and got recovered back by him'));
-
-
-    // ///////////// WORKS /////////////
-    // const tokenizerURL = chrome.runtime.getURL("model/tokenizer.json");
-
-    // let tokenizer = new WordPieceTokenizer()
-    // await tokenizer.load(tokenizerURL)
-
-    // let inputIds = tokenizer.call("telegram me at scammer.com")
-    // console.log('inputIds', inputIds);
-    // /////////////////////////////////
-
-    // const modelURL = chrome.runtime.getURL("model/model.onnx");
-
-
-    // console.log('Loading session from', modelURL);
-    // const response = await fetch(modelURL, { cache: 'force-cache' });
-    // const modelBuffer = await response.arrayBuffer();
-    // const session = await ort.InferenceSession.create(modelBuffer, {
-    //     executionProviders: ["wasm"]
-    // });
-    // console.log('Session loaded from', modelURL);
-    // console.log('session', session);
-    // console.log('session.forward',);
-
-    // let model_input = create_model_input(inputIds);
-    // let output = await session.run(model_input)
-
-    // console.log('output', output);
-    // return session;
-})();
 
 function indexOfMax(arr) {
     // https://stackoverflow.com/a/11301464
@@ -165,28 +121,3 @@ function indexOfMax(arr) {
 
     return maxIndex;
 }
-
-let modelPromise = loadModel();
-
-
-// Load the tokenizer and model.
-
-
-
-// const model = await AutoModelForSeq2SeqLM.fromPretrained("t5-small", "model");
-// const outputTokenIds = await model.generate(inputTokenIds, {maxLength:50,topK:10});
-// const french = tokenizer.decode(outputTokenIds, true);
-// console.log(french); // "Bonjour monde!"
-
-
-// (() => {
-//     loadModels().then(function(data){
-
-//     });
-// })();
-
-
-// async function loadModels() {
-//     const tfmBuffer = await(await fetch('model.onnx')).arrayBuffer()
-//     const tfmSessionPromise = await ort.InferenceSession.create(tfmBuffer, { executionProviders: ["wasm"] });
-// }
