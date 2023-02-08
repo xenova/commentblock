111b021099e8ffdf3ed0d49e16967c1d5724ebc8
Joshua Lochner <admin@xenova.com>
2023-02-08 22:19:24 +0200

    Specify extension defaults

---
+++ extension/scripts/defaults.js
@@ -0,0 +1,41 @@
+// Script that is run first
+// Sets DEFAULT storage values if not set
+
+const DEFAULT_OPTIONS = {
+
+    // CATEGORIES:
+    // 1. SCAM
+    "rules-enabled-scam": true,
+    "rules-scam-names": true,
+    "rules-scam-text": true,
+
+    // 2. EXPLICIT
+    "rules-enabled-explicit": true,
+
+    // 3. LINKS
+    "rules-enabled-links": true,
+    "rules-links-spam": true,
+    "rules-links-contains": false,
+    "rules-links-only": false,
+
+    // 4. SELF_PROMO
+    "rules-enabled-selfpromo": false,
+
+    // 5. OTHER_PROMO
+    "rules-enabled-otherpromo": false,
+
+    // 6. SPONSOR
+    "rules-enabled-sponsor": false,
+
+    // 7. OTHER_SPAM
+    "rules-enabled-spam": true,
+    "rules-spam-wafflehouse": true,
+
+    // Actions
+    "action": 'remove',
+
+
+    // OTHER SETTINGS:
+    "use-rules": true,
+    "use-ml": false,
+}
