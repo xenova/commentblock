243b4ca2a1f0aba421416df49af9f7b670f2b4a9
Joshua Lochner <admin@xenova.com>
2023-02-12 18:53:52 +0200

    Add show option for hidden comments

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -14,10 +14,28 @@ const LABEL_RULES_MAPPING = {
     [COMMENT_LABEL.OTHER_SPAM]: "rules-enabled-spam",
 }
 
+const BLURRED_COMMENT_OVERLAY_CLASS = 'blurred-comment-overlay'
+
+const LISTBOX_SELECTOR = 'tp-yt-paper-listbox#items.ytd-menu-popup-renderer';
+const LISTBOX_TEXT_SELECTOR = 'yt-formatted-string.ytd-menu-service-item-renderer';
 
 const COMMENT_TAG = 'YTD-COMMENT-RENDERER';
 const COMMENT_THREAD_TAG = 'YTD-COMMENT-THREAD-RENDERER';
 const COMMENT_TEXT = 'yt-formatted-string#content-text.ytd-comment-renderer';
+
+const MENU_ITEM = htmlToElement(
+    `<ytd-menu-service-item-renderer class="style-scope ytd-menu-popup-renderer" use-icons="" system-icons="" role="menuitem" tabindex="-1" aria-selected="false">
+        <tp-yt-paper-item class="style-scope ytd-menu-service-item-renderer" style-target="host" role="option" tabindex="0" aria-disabled="false">
+            <yt-icon class="style-scope ytd-menu-service-item-renderer"></yt-icon>
+            <yt-formatted-string class="style-scope ytd-menu-service-item-renderer"></yt-formatted-string>
+            <ytd-badge-supported-renderer class="style-scope ytd-menu-service-item-renderer" disable-upgrade="" hidden=""></ytd-badge-supported-renderer>
+        </tp-yt-paper-item>
+    </ytd-menu-service-item-renderer>`
+);
+
+
+let PREDICTION_CACHE = {};
+let focusedComment = null;
 
 (() => {
 
@@ -26,21 +44,28 @@ const COMMENT_TEXT = 'yt-formatted-string#content-text.ytd-comment-renderer';
     // observe changes on the page, comments are loaded separately so we need this to wait for them
     let observer = new MutationObserver((mutations) => {
         mutations.forEach(async (mutation) => {
-            // A child node has been added.
-            if (mutation.addedNodes.length === 0) return;
+            if (mutation.removedNodes.length > 0) {
+                let listbox;
+                if (mutation.target.matches(LISTBOX_SELECTOR)) {
+                    listbox = mutation.target;
+                } else if (mutation.target.matches(LISTBOX_TEXT_SELECTOR)) {
+                    listbox = mutation.target.closest(LISTBOX_SELECTOR);
+                }
 
-            // For optimisation purposes, YouTube doesn't remove-then-insert new comments.
-            // Instead, they replace content of existing elmenets on the page.
-            // For this reason, we have to listen for changes to the actual text content
-            // of the comment, then crawl back up to the actual comment element.
-            // This is especially needed when sorting by recent
+                if (listbox && listbox.childNodes.length == 3) {
+                    addHideOption(listbox);
+                }
 
-            if (!mutation.target.matches(COMMENT_TEXT)) return;
-
-            let comment = mutation.target.closest(COMMENT_TAG);
-            if (comment === null) return;
-            processComment(comment);
-
+            } else if (mutation.addedNodes.length > 0 && mutation.target.matches(COMMENT_TEXT)) { // is a comment
+                // For optimisation purposes, YouTube doesn't remove-then-insert new comments.
+                // Instead, they replace content of existing elmenets on the page.
+                // For this reason, we have to listen for changes to the actual text content
+                // of the comment, then crawl back up to the actual comment element.
+                // This is especially needed when sorting by recent
+                let comment = mutation.target.closest(COMMENT_TAG);
+                if (comment === null) return;
+                processComment(comment);
+            }
         })
     });
 
@@ -52,7 +77,56 @@ const COMMENT_TEXT = 'yt-formatted-string#content-text.ytd-comment-renderer';
     });
 })();
 
-let PREDICTION_CACHE = {};
+function addBlur(commentElement) {
+    let overlay = document.createElement('div');
+    overlay.className = BLURRED_COMMENT_OVERLAY_CLASS;
+    commentElement.append(overlay);
+}
+
+function getBlur(commentElement) {
+    return commentElement.querySelector(`div.${BLURRED_COMMENT_OVERLAY_CLASS}`);
+}
+
+function isBlurred(commentElement) {
+    return getBlur(commentElement) !== null;
+}
+
+function removeBlur(commentElement) {
+    let overlay = getBlur(commentElement);
+    if (overlay) overlay.remove();
+}
+
+function addHideOption(listbox) {
+    // TODO: Add option to hide comments?
+    // let commentIsBlurred = isBlurred(focusedComment);
+
+    let elem = MENU_ITEM.cloneNode();
+    listbox.appendChild(elem)
+
+    elem.addEventListener('click', (e) => {
+        removeBlur(focusedComment);
+
+        // Simulate click elsewhere to unfocus
+        document.body.click();
+    })
+
+    // Must do it this way for some reason?
+    // YouTube seems to override yt-icon when appending?
+    elem.querySelector('yt-icon').innerHTML =
+        `<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+            <g class="style-scope yt-icon">
+            <path d="M12 15.15q1.525 0 2.588-1.063 1.062-1.062 1.062-2.587 0-1.525-1.062-2.588Q13.525 7.85 12 7.85q-1.525 0-2.587 1.062Q8.35 9.975 8.35 11.5q0 1.525 1.063 2.587Q10.475 15.15 12 15.15Zm0-.95q-1.125 0-1.912-.788Q9.3 12.625 9.3 11.5t.788-1.913Q10.875 8.8 12 8.8t1.913.787q.787.788.787 1.913t-.787 1.912q-.788.788-1.913.788Zm0 3.8q-3.25 0-5.925-1.762-2.675-1.763-4-4.738 1.325-2.975 4-4.738Q8.75 5 12 5t5.925 1.762q2.675 1.763 4 4.738-1.325 2.975-4 4.738Q15.25 18 12 18Zm0-6.5Zm0 5.5q2.825 0 5.188-1.488Q19.55 14.025 20.8 11.5q-1.25-2.525-3.612-4.013Q14.825 6 12 6 9.175 6 6.812 7.487 4.45 8.975 3.2 11.5q1.25 2.525 3.612 4.012Q9.175 17 12 17Z" class="style-scope yt-icon"></path>
+            </g>
+        </svg>`;
+
+    // visibility off
+    // https://fonts.google.com/icons?icon.query=show
+
+    // Add text
+    let t = elem.querySelector('yt-formatted-string');
+    t.removeAttribute('is-empty');
+    t.textContent = 'Show';
+}
 
 async function processComment(comment) {
 
@@ -75,6 +149,13 @@ async function processComment(comment) {
     let authorName = authorData.innerText;
     let commentText = extractTextFromElement(comment.querySelector('#comment-content #content-text'));
     let authorChannelId = authorData.href.replace('https://www.youtube.com/channel/', '');
+
+
+    // Add event listener to options
+    let actionButton = comment.querySelector('#action-menu.ytd-comment-renderer button')
+    actionButton.addEventListener('click', () => {
+        focusedComment = comment;
+    })
 
     // Set data attributes
     comment.data = {
@@ -108,9 +189,11 @@ function extractTextFromElement(element) {
 function show(element) {
     element.style.display = 'block';
 }
+
 function hide(element) {
     element.style.display = 'none';
 }
+
 async function action(comment, prediction) {
 
     // Check if the predicted category is enabled
@@ -141,13 +224,10 @@ async function action(comment, prediction) {
 
         } else if (action === 'blur') {
             // TODO improve blurring
-            let overlay = document.createElement('div');
-            overlay.className = 'blurred-comment-overlay';
-
-            comment.append(overlay);
+            addBlur(comment);
 
         } else {
-            console.log(`Unknown action: ${action}`)
+            console.error(`Unknown action: ${action}`)
         }
     } else {
         // Reset
@@ -158,8 +238,7 @@ async function action(comment, prediction) {
         }
 
         // Remove blurred overlay if present
-        let overlay = comment.querySelector('div.blurred-comment-overlay');
-        if (overlay) overlay.remove();
+        removeBlur(comment);
 
 
     }
